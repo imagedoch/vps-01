5353d53f1cd2e5ccdc7c3160baa6e9572bf96a12
imagedoch <123003905+imagedoch@users.noreply.github.com>
2023-01-19 02:35:22 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,42 +1,37 @@
 
-<h3 align="center"><b>ShieldbladeNet</b></h3>
-<h3 align="center">Have a nice day!</h3>
-<p align="center">
-  <a href="https://github.com/ShieldbladeNet">
-    <img alt="GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=ShieldbladeNet&hide=issues&hide_title=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" />
-    </a>     
-    
-<p align="center">
+   
+#### This Page Create at:
 
-[![build_firmware](https://github.com/ShieldbladeNet/Nano-Openwrt/actions/workflows/build-firmware.yml/badge.svg)](https://github.com/ShieldbladeNet/Nano-Openwrt/actions/workflows/build-firmware.yml)
+```bash
+
+2021-06-28 - 15:21:14 UTC
+
+```
+
+#### Create By Machine:
+
+```bash
+
+Host Name : fv-az42-873
+
+platform  : Linux-5.8.0-1033-azure-x86_64-with-glibc2.31
+
+Ip Local  : 10.1.0.5
+
+```
+#### Download This code Here:
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/Chiemaeh/Java-Build?style=for-the-badge&label=Download)](https://github.com/Chiemaeh/Java-Build/releases) 
 
 </p> 
 
-# Nano R1S R2S R4S x86 Openwrt 
- 
-### Download Link：
+#### About Me :
 
-[![GitHub release (latest by date)](https://img.shields.io/github/v/release/ShieldbladeNet/Nano-Openwrt?style=for-the-badge&label=Download)](https://github.com/ShieldbladeNet/Nano-Openwrt/releases) 
+[![build](https://github.com/Chiemaeh/Java-Build/actions/workflows/build.yml/badge.svg)](https://github.com/Chiemaeh/Java-Build/actions/workflows/build.yml)
 
-(Files do not need to be decompressed, you can directly use the flashing tool to flash in img.gz)
-
-### Use suggestions:
-The default user name is root, the password is password, and the LAN IP is 192.168.2.1
-After burning the firmware, insert the tf card and start it up. After the computer displays "Network (Connected)", enter http://immortalwrt/ in the browser to directly open the router background without modifying the local connection settings or checking the IP address.
-If the network status has been unrecognized (more than 5 minutes after power-on), please plug in and unplug the power directly and restart it.
-
-### Online upgrade method in the terminal:
 ```bash
-wget -qO- https://github.com/ShieldbladeNet/Nano-Openwrt/raw/main/scripts/autoupdate.sh | sh
-```
-slim Version
-```bash
-wget -qO- https://github.com/ShieldbladeNet/Nano-Openwrt/raw/main/scripts/autoupdate.sh | ver=-slim sh
-```
-(Script provided by Gary Lau, thank you very much!)
 
-#### Firmware source code:
-https://github.com/immortalwrt/immortalwrt
+Chiemaeh
 
-<h3 align="center"><b>This Page Create at</b></h3>
-<h3 align="center"><b>2021-06-10 - 11:37:27 UTC</b></h3>
+```
+
--- generate.py
+++ generate.py
@@ -1,49 +1,52 @@
-import requests, json, random, time
+import json, random, time, socket, platform
+
 timestr = time.strftime("%Y-%m-%d - %H:%M:%S UTC")
-nameid = "ShieldbladeNet"
+
+nameid = "Chiemaeh"
+
+repoid = "Java-Build"
+
+yamlid = "build"
+
 f = open("./README.md", "w")
 
 f.write(f'''
-<h3 align="center"><b>{nameid}</b></h3>
-<h3 align="center">Have a nice day!</h3>
-<p align="center">
-  <a href="https://github.com/{nameid}">
-    <img alt="GitHub Stats" src="https://github-readme-stats.vercel.app/api?username={nameid}&hide=issues&hide_title=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" />
-    </a>     
-    
-<p align="center">
+   
+#### This Page Create at:
 
-[![build_firmware](https://github.com/{nameid}/Nano-Openwrt/actions/workflows/build-firmware.yml/badge.svg)](https://github.com/{nameid}/Nano-Openwrt/actions/workflows/build-firmware.yml)
+```bash
+
+{timestr}
+
+```
+
+#### Create By Machine:
+
+```bash
+
+Host Name : {socket.gethostname()}
+
+platform  : {platform.platform()}
+
+Ip Local  : {socket.gethostbyname(socket.gethostname())}
+
+```
+#### Download This code Here:
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/{nameid}/{repoid}?style=for-the-badge&label=Download)](https://github.com/{nameid}/{repoid}/releases) 
 
 </p> 
 
-# Nano R1S R2S R4S x86 Openwrt 
- 
-### Download Link：
+#### About Me :
 
-[![GitHub release (latest by date)](https://img.shields.io/github/v/release/{nameid}/Nano-Openwrt?style=for-the-badge&label=Download)](https://github.com/{nameid}/Nano-Openwrt/releases) 
+[![{yamlid}](https://github.com/{nameid}/{repoid}/actions/workflows/{yamlid}.yml/badge.svg)](https://github.com/{nameid}/{repoid}/actions/workflows/{yamlid}.yml)
 
-(Files do not need to be decompressed, you can directly use the flashing tool to flash in img.gz)
-
-### Use suggestions:
-The default user name is root, the password is password, and the LAN IP is 192.168.2.1
-After burning the firmware, insert the tf card and start it up. After the computer displays "Network (Connected)", enter http://immortalwrt/ in the browser to directly open the router background without modifying the local connection settings or checking the IP address.
-If the network status has been unrecognized (more than 5 minutes after power-on), please plug in and unplug the power directly and restart it.
-
-### Online upgrade method in the terminal:
 ```bash
-wget -qO- https://github.com/{nameid}/Nano-Openwrt/raw/main/scripts/autoupdate.sh | sh
-```
-slim Version
-```bash
-wget -qO- https://github.com/{nameid}/Nano-Openwrt/raw/main/scripts/autoupdate.sh | ver=-slim sh
-```
-(Script provided by Gary Lau, thank you very much!)
 
-#### Firmware source code:
-https://github.com/immortalwrt/immortalwrt
+{nameid}
 
-<h3 align="center"><b>This Page Create at</b></h3>
-<h3 align="center"><b>{timestr}</b></h3>
+```
+
 ''')
+
 f.close()
